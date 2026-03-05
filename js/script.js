2f010a53c655e8efe52a77b69a4876a6bce7b748
Nghia21102004 <nghiaz2110@gmail.com>
2026-03-05 23:20:07 +0700

    auto update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,89 @@
-function showPage(id){
 
-let pages = document.querySelectorAll(".page")
+let revenue = 0
+let cost = 0
 
-pages.forEach(page => {
+function updateUI(){
 
-page.classList.remove("active")
+document.getElementById("todayRevenue").innerText = revenue
+document.getElementById("todayCost").innerText = cost
+document.getElementById("todayProfit").innerText = revenue - cost
 
-})
-
-document.getElementById(id).classList.add("active")
+updateChart()
 
 }
 
-showPage("home")
+function addRevenue(){
+
+let value = Number(document.getElementById("sellInput").value)
+
+revenue += value
+
+saveHistory("Doanh thu +" + value)
+
+updateUI()
+
+}
+
+function addCost(){
+
+let value = Number(document.getElementById("costInput").value)
+
+cost += value
+
+saveHistory("Chi phí -" + value)
+
+updateUI()
+
+}
+
+function showPage(page){
+
+let pages = document.querySelectorAll(".page")
+
+pages.forEach(p=>{
+p.classList.remove("active")
+})
+
+document.getElementById(page).classList.add("active")
+
+}
+
+function saveHistory(text){
+
+let list = document.getElementById("historyList")
+
+let li = document.createElement("li")
+
+li.innerText = text
+
+list.prepend(li)
+
+}
+
+let chart
+
+function updateChart(){
+
+let data = [revenue,cost,revenue-cost]
+
+if(chart){
+chart.data.datasets[0].data = data
+chart.update()
+return
+}
+
+let ctx = document.getElementById("myChart")
+
+chart = new Chart(ctx,{
+type:"bar",
+data:{
+labels:["Doanh thu","Chi phí","Lợi nhuận"],
+datasets:[{
+data:data
+}]
+}
+})
+
+}
+
+updateUI()
